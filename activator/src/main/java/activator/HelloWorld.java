ae828d63ddbac5d84f3bfb9aa06d59ef41b9daf4
Alexey Seliverstov <alexeyseliverstovigorevich@gmail.com>
2020-09-13 20:24:37 +0300

    with JUNIT

--- activator/src/main/java/activator/HelloWorld.java
+++ activator/src/main/java/activator/HelloWorld.java
@@ -2,7 +2,10 @@ package activator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+
+
 public class HelloWorld implements BundleActivator{
+
     public void start(BundleContext bundleContext) throws Exception {
         System.out.println("Hello World!");
     }
